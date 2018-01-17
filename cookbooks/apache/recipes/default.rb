7fefff25f2bbda6b559e130a19b57f5e8c02c16b
Ranjith Kumar <tranjith23@gmail.com>
2018-01-17 18:58:57 +0530

    chef-repo fist commit

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,11 @@
+package "httpd" do
+action :install
+end
+service "httpd" do
+action [ :enable, :start ]
+end
+node.default["apache"]["indexfile"] = "samp.html"
+cookbook_file "/var/www/html/index.html" do
+source node["apache"]["indexfile"]
+mode "0644"
+end
